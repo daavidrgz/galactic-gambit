142efa3817dc92043dd7da243331b555ed48fa75
Jan Duinkerken Rodriguez <jan.duinkerken@udc.es>
2023-02-16 22:23:06 +0100

    Control system

--- src/control_system.py
+++ src/control_system.py
@@ -1,13 +1,28 @@
-from enum import Enum
+from enum import Enum, auto
 
 # Enum de las acciones que hay
+# Acciones posibles:
+#   Disparar
 
 
-class Actions(Enum):
-    pass
+class ActionsEnum(Enum):
+    DISPARAR = auto()
 
+class Actions:
+    __instance = None
 
-# Funcion que recibe un valor del enum, y te devuelva si esta pulsado o no
-# Se puede hacer el polling cada vez que se llame a esa funcion
+    def __init__(self):
+        self.actions = {ActionsEnum.DISPARAR:"A"}
 
-# Funcion para bindear una tecla a un enum,y que se guarde en un mapa
+    # Funcion que recibe un valor del enum, y te devuelva si esta pulsado o no
+    # Se puede hacer el polling cada vez que se llame a esa funcion
+    def isActiveAction(self,action):
+        keys=pygame.key.get_pressed()
+        if keys[self.actions[action.name()]]:
+            return True
+        else:
+            return False
+
+    # Funcion para bindear una tecla a un enum,y que se guarde en un mapa
+    def rebindAction(self, action, key):
+        self.actions[action.name()] = key
