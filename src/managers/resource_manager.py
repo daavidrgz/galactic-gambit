50ff1ffb3e08f6eb3226ae4e96e397c473336660
Jan Duinkerken Rodríguez <jan.duinkerken@udc.es>
2023-02-18 11:57:29 +0100

    Changes to resource manager

--- src/managers/resource_manager.py
+++ src/managers/resource_manager.py
@@ -1,42 +1,65 @@
 import pygame
 import os
 
-IMAGES_PATH = "assets/sprites"
-SOUNDS_PATH = "assets/sounds"
-COORDINATES_PATH = "coordenadas"
-
 
 class ResourceManager(object):
+    __instance = None
     resources = {}
 
+    BASE_PATH = "assets"
+
+    # Images
+    PLAYER = "sprites/player.png"
+
+    # Sounds (I dont know which ones we will use)
+
+    # Coordinates (Will we use them?)
+
+    def get_instance():
+        if ResourceManager.__instance is None:
+            ResourceManager.__instance = ResourceManager()
+        return ResourceManager.__instance
+
     @classmethod
-    def loadImage(cls, name):
-        if name in cls.resources:
-            return cls.resources[name]
+    def loadImage(self, name):
+        if name in self.resources:
+            return self.resources[name]
         else:
-            fullname = os.path.join(IMAGES_PATH, name)
-            image = pygame.image.load(fullname)
-            cls.resources[name] = image
+            fullname = os.path.join(self.BASE_PATH, name)
+            try:
+                image = pygame.image.load(fullname)
+            except (pygame.error):
+                print("Error loading image: ", fullname)
+                raise SystemExit
+            self.resources[name] = image
             return image
 
     @classmethod
-    def loadSound(cls, name):
-        if name in cls.resources:
-            return cls.resources[name]
+    def loadSound(self, name):
+        if name in self.resources:
+            return self.resources[name]
         else:
-            fullname = os.path.join(SOUNDS_PATH, name)
-            sound = pygame.mixer.Sound(fullname)
-            cls.resources[name] = sound
+            fullname = os.path.join(self.BASE_PATH, name)
+            try:
+                sound = pygame.mixer.Sound(fullname)
+            except (pygame.error):
+                print("Error loading sound: ", fullname)
+                raise SystemExit
+            self.resources[name] = sound
             return sound
 
     @classmethod
-    def loadCoordinatesFile(cls, name):
-        if name in cls.resources:
-            return cls.resources[name]
+    def loadCoordinatesFile(self, name):
+        if name in self.resources:
+            return self.resources[name]
         else:
-            fullname = os.path.join(COORDINATES_PATH, name)
-            pfile = open(fullname, "r")
-            datos = pfile.read()
-            pfile.close()
-            cls.resources[name] = datos
+            fullname = os.path.join(self.BASE_PATH, name)
+            try:
+                pfile = open(fullname, "r")
+                datos = pfile.read()
+                pfile.close()
+            except (pygame.error):
+                print("Error loading coord file: ", fullname)
+                raise SystemExit
+            self.resources[name] = datos
             return datos
