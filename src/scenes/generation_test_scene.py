13121cbbca280cae29fe29ae3471a1f7881857a0
Suso <jesusmosteiro@gmail.com>
2023-02-18 21:22:36 +0100

    Update generation test scene

--- src/scenes/generation_test_scene.py
+++ src/scenes/generation_test_scene.py
@@ -1,13 +1,14 @@
 from scenes.scene import Scene
 from generation.generator import BaseGenerator
 from control_system import ControlSystem, Actions
+from managers.resource_manager import ResourceManager
 
 import pygame
 import numpy as np
-from constants import TILE_SIZE
+from constants import TILE_SIZE, DESIGN_WIDTH, DESIGN_HEIGHT
 
-CAMERAX = 0
-CAMERAY = 0
+CAMERAX = 114.5 * TILE_SIZE - DESIGN_WIDTH / 2
+CAMERAY = 114.5 * TILE_SIZE - DESIGN_HEIGHT / 2
 
 class TestScrollGroup(pygame.sprite.Group):
     def draw(self, surface):
@@ -31,11 +32,15 @@ class TestScrollGroup(pygame.sprite.Group):
 
 class TestGenerator(BaseGenerator):
     def __init__(self, collide_grp, pass_grp):
-        super().__init__(np.full((230, 230), False), (11, 11), collide_grp, pass_grp, (10.0, 10.0), (5, 5))
+        self.resource_manager = ResourceManager.get_instance()
+        self.player_sprite = pygame.transform.scale(self.resource_manager.load_image(self.resource_manager.PLAYER), (32, 32))
+        self.cobble_sprite = pygame.transform.scale(self.resource_manager.load_image(self.resource_manager.COBBLESTONE), (32, 32))
+
+        super().__init__(np.full((231, 231), False), (114, 114), collide_grp, pass_grp, (10.0, 20.0), (7, 5))
 
     def get_wall_sprite(self, x, y):
         new_tile = pygame.sprite.Sprite()
-        new_tile.image = pygame.transform.scale(pygame.image.load("assets/sprites/cobblestone.png"), (32, 32))
+        new_tile.image = self.cobble_sprite
         new_tile.rect = new_tile.image.get_rect()
         new_tile.rect.centerx = x * TILE_SIZE + 16
         new_tile.rect.centery = y * TILE_SIZE + 16
@@ -43,7 +48,7 @@ class TestGenerator(BaseGenerator):
     
     def get_ground_sprite(self, x, y):
         new_tile = pygame.sprite.Sprite()
-        new_tile.image = pygame.transform.scale(pygame.image.load("assets/sprites/player.png"), (32, 32))
+        new_tile.image = self.player_sprite
         new_tile.rect = new_tile.image.get_rect()
         new_tile.rect.centerx = x * TILE_SIZE + 16
         new_tile.rect.centery = y * TILE_SIZE + 16
@@ -56,14 +61,13 @@ class GenerationScene(Scene):
         self.ground_group = TestScrollGroup()
         self.wall_group = TestScrollGroup()
         self.control = ControlSystem.get_instance()
-        TestGenerator(self.wall_group, self.ground_group).generate()
 
     def update(self, elapsed_time):
         global CAMERAX, CAMERAY
-        if self.control.isActiveAction(Actions.ARRIBA): CAMERAY -= 5
-        if self.control.isActiveAction(Actions.ABAJO): CAMERAY += 5
-        if self.control.isActiveAction(Actions.IZQUIERDA): CAMERAX -= 5
-        if self.control.isActiveAction(Actions.DERECHA): CAMERAX += 5
+        if self.control.is_active_action(Actions.UP   ): CAMERAY -= 5
+        if self.control.is_active_action(Actions.DOWN ): CAMERAY += 5
+        if self.control.is_active_action(Actions.LEFT ): CAMERAX -= 5
+        if self.control.is_active_action(Actions.RIGHT): CAMERAX += 5
 
     def handle_events(self, events):
         for event in events:
@@ -73,7 +77,10 @@ class GenerationScene(Scene):
                 if event.key == pygame.K_SPACE:
                     self.ground_group.empty()
                     self.wall_group.empty()
+                    import time
+                    a = time.time()
                     TestGenerator(self.wall_group, self.ground_group).generate()
+                    print(time.time() - a)
 
     def draw(self, screen):
         BLACK = (0, 0, 0)
