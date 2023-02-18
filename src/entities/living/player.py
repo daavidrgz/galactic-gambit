719852d43eef7fe9a6f29fde3eb470b3e3ceae21
Jan Duinkerken Rodríguez <jan.duinkerken@udc.es>
2023-02-18 13:03:16 +0100

    Loading sprites using resource manager

--- src/entities/living/player.py
+++ src/entities/living/player.py
@@ -1,10 +1,12 @@
 from entities.living.living_entity import LivingEntity
+from managers.resource_manager import ResourceManager
 import pygame
 
 
 class Player(LivingEntity):
     def __init__(self, initial_pos):
-        image = pygame.image.load("assets/sprites/player.png")
+        self.manager = ResourceManager.get_instance()
+        image = self.manager.loadImage(self.manager.PLAYER)
         hitbox = image.get_rect()
         super().__init__(image, hitbox, initial_pos, 100)
 
--- src/entities/projectile/bullet.py
+++ src/entities/projectile/bullet.py
@@ -1,10 +1,12 @@
 from entities.projectile.projectile import Projectile
+from managers.resource_manager import ResourceManager
 import pygame
 
 
 class Bullet(Projectile):
     def __init__(self, initial_pos, speed, direction):
-        image = pygame.image.load("assets/sprites/player.png")
+        self.manager = ResourceManager.get_instance()
+        image = self.manager.loadImage(self.manager.PLAYER)
         scaled_image = pygame.transform.scale(image, (16, 16))
         hitbox = scaled_image.get_rect()
         super().__init__(scaled_image, hitbox, initial_pos, speed, direction)
