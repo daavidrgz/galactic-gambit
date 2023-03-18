cf2bb477e7146f697454835b3a4f4917df274195
Suso <jesusmosteiro@gmail.com>
2023-03-18 14:20:10 +0100

    Nicer populate terrain code

--- src/scenes/levels/cave/cave_terrain.py
+++ src/scenes/levels/cave/cave_terrain.py
@@ -13,14 +13,16 @@ class CaveTerrain(BaseTerrain):
         self.height, self.width = self.data.shape
         self.starting_tiles = [(84, 7), (85, 7), (86, 7)]
 
+        # Starting ground and upper wall
         self.data[4:7, 84:87] = TerrainType.GROUND
         self.data[4, 80:90] = TerrainType.WALL
 
         self.player_starting_position = (TILE_SIZE * 86.0, TILE_SIZE * 5.5)
 
-        self.place_start_sprite(82, 2)
+        self.place_start_sprite(81, 0)
 
     def place_end(self, end_pos):
+        # Make a small area for the end
         for x in range(end_pos[0] - 7, end_pos[0] + 8):
             for y in range(end_pos[1] - 7, end_pos[1] + 8):
                 distance_sqr = (x // 2 * 2 - end_pos[0]) ** 2 + (
@@ -32,20 +34,15 @@ class CaveTerrain(BaseTerrain):
         self.data[
             end_pos[1] : end_pos[1] + 2, end_pos[0] : end_pos[0] + 2
         ] = TerrainType.WALL
+        
         self.end_position = (
             (end_pos[0] + 0.5) * TILE_SIZE,
             (end_pos[1] + 0.5) * TILE_SIZE,
         )
 
     def place_start_sprite(self, x, y):
-        init_room_spr = pygame.sprite.Sprite()
-        init_room_spr.image = ResourceManager().load_image(Resource.CAVE_START)
-        init_room_spr.image = pygame.transform.scale(
-            init_room_spr.image, [a * 2 for a in init_room_spr.image.get_size()]
+        image = ResourceManager().load_image(Resource.CAVE_START)
+        image = pygame.transform.scale(
+            image, [a * 2 for a in image.get_size()]
         )
-        init_room_spr.rect = init_room_spr.image.get_rect()
-        init_room_spr.rect.topleft = (
-            (x - 1) * TILE_SIZE,
-            (y - 2) * TILE_SIZE,
-        )
-        self.sprites_top.add(init_room_spr)
+        self.place_top_sprite(x, y, image)
--- src/scenes/levels/ship/ship_terrain.py
+++ src/scenes/levels/ship/ship_terrain.py
@@ -19,6 +19,8 @@ class ShipTerrain(BaseTerrain):
             TerrainType.NONE,
             dtype=np.int16,
         )
+
+        # Level bounds
         self.data = np.pad(
             data,
             ((self.Y_SIZE * 2, self.Y_SIZE), (self.X_SIZE, self.X_SIZE)),
@@ -27,6 +29,7 @@ class ShipTerrain(BaseTerrain):
         )
         self.height, self.width = self.data.shape
 
+        # Choose random X for starting room
         rng = RngSystem().get_rng(Generator.MAP)
         self.start_room_x = rng.randint(1, self.X_ROOM)
 
@@ -35,6 +38,7 @@ class ShipTerrain(BaseTerrain):
         base_y = self.height - self.Y_SIZE * 2
         end_y = base_y + self.Y_SIZE
 
+        # Starting room collision
         self.data[base_y - 2 : end_y + 2, base_x - 3 : end_x + 3] = TerrainType.BOUND
         self.data[base_y - 1 : end_y + 1, base_x - 1 : end_x + 1] = TerrainType.WALL
         self.data[base_y:end_y, base_x:end_x] = TerrainType.GROUND
@@ -48,11 +52,13 @@ class ShipTerrain(BaseTerrain):
             TILE_SIZE * (base_y + 2.5),
         )
 
-        self.place_start_sprite(base_x, base_y)
+        self.place_start_sprite(base_x - 1, base_y - 2)
 
     def place_end(self, end_coords):
         room_x = end_coords[0] // self.X_SIZE
         room_y = (end_coords[1] + 1) // self.Y_SIZE - 1
+
+        # Ensure we place this up from a wall
         if end_coords[0] % self.X_SIZE == self.X_SIZE - 1:
             room_x += 1
 
@@ -61,6 +67,7 @@ class ShipTerrain(BaseTerrain):
         base_y = room_y * self.Y_SIZE
         end_y = base_y + self.Y_SIZE
 
+        # Add floor to stand on
         self.data[base_y:end_y, base_x:end_x] = TerrainType.GROUND
 
         self.end_position = (
@@ -68,27 +75,15 @@ class ShipTerrain(BaseTerrain):
             (base_y + 1.5) * TILE_SIZE,
         )
 
-        self.place_end_sprite(base_x + 1, base_y + 2)
+        self.place_end_sprite(base_x, base_y)
 
     def place_start_sprite(self, x, y):
-        init_room_spr = pygame.sprite.Sprite()
-        init_room_spr.image = ResourceManager().load_image(Resource.SHIP_START)
-        init_room_spr.rect = init_room_spr.image.get_rect()
-        init_room_spr.rect.topleft = (
-            (x - 1) * TILE_SIZE,
-            (y - 2) * TILE_SIZE,
-        )
-        self.sprites_top.add(init_room_spr)
+        image = ResourceManager().load_image(Resource.SHIP_START)
+        self.place_top_sprite(x, y, image)
 
     def place_end_sprite(self, x, y):
-        end_room_spr = pygame.sprite.Sprite()
-        end_room_spr.image = ResourceManager().load_image(Resource.SHIP_END)
-        end_room_spr.image = pygame.transform.scale(
-            end_room_spr.image, [a * 2 for a in end_room_spr.image.get_size()]
+        image = ResourceManager().load_image(Resource.SHIP_END)
+        image = pygame.transform.scale(
+            image, [a * 2 for a in image.get_size()]
         )
-        end_room_spr.rect = end_room_spr.image.get_rect()
-        end_room_spr.rect.topleft = (
-            (x - 1) * TILE_SIZE,
-            (y - 2) * TILE_SIZE,
-        )
-        self.sprites_top.add(end_room_spr)
+        self.place_top_sprite(x, y, image)
