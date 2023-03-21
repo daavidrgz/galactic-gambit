b5d4f41c46f7e6a9ffa1fd41e68719232ed14c41
Jorge Hermo <jorge.hermo.gonzalez@udc.es>
2023-03-21 21:47:46 +0100

    fix collide func

--- src/entities/projectile/projectile.py
+++ src/entities/projectile/projectile.py
@@ -23,7 +23,7 @@ class Projectile(Entity):
     def setup(self, level):
         super().setup(level)
 
-    def collide(self):
+    def collide(self,add_animation_func):
         self.kill()
 
     def set_facing(self, angle):
